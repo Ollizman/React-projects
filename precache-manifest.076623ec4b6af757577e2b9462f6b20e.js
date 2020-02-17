0234ae8298b83ef2d128b3a9fa2912354b81d2a8
Manninen Olli <s1900562@edu.bc.fi>
2020-02-17 11:54:41 +0200

    Updates

---
+++ precache-manifest.076623ec4b6af757577e2b9462f6b20e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94261fbbdf101bfe74705d36283af206",
+    "url": "/React-projects/index.html"
+  },
+  {
+    "revision": "f3436f09fa4f5de81fe2",
+    "url": "/React-projects/static/css/main.0cc818aa.chunk.css"
+  },
+  {
+    "revision": "90019cbf2f0e7e7cae03",
+    "url": "/React-projects/static/js/2.23115797.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React-projects/static/js/2.23115797.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3436f09fa4f5de81fe2",
+    "url": "/React-projects/static/js/main.db057bee.chunk.js"
+  },
+  {
+    "revision": "f7cfd226811cdf3f2d29",
+    "url": "/React-projects/static/js/runtime-main.8e1aff41.js"
+  }
+]);
